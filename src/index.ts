c8bd6e0104a651f0a3410f55e1c16d1d39963672
dm-mrtnvch <dmvshn@gmail.com>
2022-05-15 22:58:16 +0400

    before routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from "express";
 import cors from 'cors'
 import bodyParser from "body-parser";
+import {videosRepository} from "./repositories/videos-repository";
 
 const app = express()
 app.use(cors())
@@ -13,25 +14,26 @@ interface Ivideo {
     author: string
 }
 
-let videos: Ivideo[] = [
-    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
-    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
-    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
-    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
-    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
-]
+// let videos: Ivideo[] = [
+//     {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
+//     {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
+//     {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
+//     {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
+//     {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
+// ]
 
 app.get('/', (req: Request, res: Response) => {
     res.send('hello')
 })
 
 app.get('/videos', (req: Request, res: Response) => {
-    res.send(videos)
+    res.send(videosRepository.getVideos())
 })
 
 app.get('/videos/:videoId', (req, res) => {
     const id = +req.params.videoId
-    const video = videos.find(v => v.id === id)
+    const video = videosRepository.getVideoById(id)
+
     if (video) {
         res.send(video)
     } else {
@@ -40,29 +42,23 @@ app.get('/videos/:videoId', (req, res) => {
 })
 
 app.post('/videos', (req, res) => {
-    if(!req.body.title || req.body.title.length > 40) {
-        res.status(400).send({ errorsMessages: [{ message: "string", field: "title" }], resultCode: 1 })
-        return
+    if (req.body.title || req.body.title.length < 40) {
+        const newVideo = videosRepository.createVideo(req.body.title)
+        res.status(201).send(newVideo)
+    } else {
+        res.status(400).send({errorsMessages: [{message: "string", field: "title"}], resultCode: 1})
     }
-    const newVideo: Ivideo = {
-        id: videos[videos.length - 1].id + 1,
-        title: req.body.title,
-        author: videos[0].author
-    }
-    videos = [...videos, newVideo]
-    res.status(201).send(newVideo)
 })
 
 app.put('/videos/:videoId', (req: Request, res: Response) => {
-    if(!req.body.title || req.body.title.length > 40) {
-        res.status(400).send({ errorsMessages: [{ message: "string", field: "title" }], resultCode: 1 })
+    if (!req.body.title || req.body.title.length > 40) {
+        res.status(400).send({errorsMessages: [{message: "string", field: "title"}], resultCode: 1})
         return
     }
     const id = +req.params.videoId
-    const video = videos.find(v => v.id === id)
-    if(video) {
-        video.title = req.body.title
-        res.status(204).send(video)
+    const updatedVideo = videosRepository.updateVideoById(id, req.body.title)
+    if (updatedVideo  ) {
+        res.send(204)
     } else {
         res.send(404)
     }
@@ -70,9 +66,8 @@ app.put('/videos/:videoId', (req: Request, res: Response) => {
 
 app.delete('/videos/:videoId', (req, res) => {
     const id = +req.params.videoId
-    const filteredVideos = videos.filter(v => v.id !== id)
-    if (videos.length > filteredVideos.length) {
-        videos = filteredVideos
+    const isDeleted = videosRepository.deleteVideoById(id)
+    if (isDeleted) {
         res.send(204)
     } else {
         res.send(404)
---
+++ src/repositories/videos-repository.ts
@@ -0,0 +1,57 @@
+export interface Ivideo {
+    id: number,
+    title: string,
+    author: string
+}
+
+
+export let videos: Ivideo[] = [
+    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
+    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
+    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
+    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
+    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
+]
+
+export const videosRepository = {
+    getVideos() {
+        return videos
+    },
+    getVideoById(id: number) {
+        return videos.find(v => v.id === id)
+    },
+    deleteVideoById(id: number) {
+        const filteredVideos = videos.filter(v => v.id !== id)
+        if(filteredVideos.length < videos.length){
+            videos = filteredVideos
+            return true
+        } else {
+            return false
+        }
+    },
+    updateVideoById(id: number, title: string) {
+        const video = videos.find(v => v.id === id)
+
+        if(video) {
+            return {
+                ...video,
+                title: title
+            }
+        } else {
+            return null
+        }
+    },
+    createVideo(title: string) {
+        if(title) {
+            const newVideo: Ivideo = {
+                id: 12,
+                title: 'blablabla',
+                author: 'author bla'
+            }
+            videos.push(newVideo)
+            return true
+        } else {
+            return false
+        }
+    }
+}
